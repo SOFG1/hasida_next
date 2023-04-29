b7e0312fe9a488da9cdfd7c1f220eebf099b00fd
Eddy <edkosyan2000@gmail.com>
2023-04-29 17:19:35 +0400

    add translations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import "../translations/i18init";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
