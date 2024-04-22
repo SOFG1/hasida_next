5ef880bb759727810b8d2fdcb0c55048c51085df
Eddy <edkosyan2000@gmail.com>
2024-04-22 17:44:23 +0400

    make text bigger

--- src/views/HomeViews/CandidatesView.tsx
+++ src/views/HomeViews/CandidatesView.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { homeAppliedFiltersSelector } from "../../store/home";
 import { Loader } from "../../UI/Loader";
 import CandidateCard from "./CandidateCard";
-import { useTranslation } from "react-i18next"; 
+import { useTranslation } from "react-i18next";  
 import { useLazyProfilesListQuery } from "../../api/home";
 import { convertFiltersData } from "../../utils";  
                 
