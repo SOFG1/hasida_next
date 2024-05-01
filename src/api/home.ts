20973ceca1eba2cd7af83f998fc17b717c9f3b02
Eddy <edkosyan2000@gmail.com>
2024-05-02 00:59:02 +0400

    fix bug

--- src/api/home.ts
+++ src/api/home.ts
@@ -96,7 +96,7 @@ export const homeApi = emptyApi.injectEndpoints({
             method: "POST", 
           };        
         },     
-      }),             
+      }),              
     };              
   },                
 });                       
